a86bcecde9d1cda55c53a03b868f478af1439650
Jaroslav <music.jp.production@gmail.com>
2026-06-06 12:53:58 +0300

    Embedded a few more icons from FontAwesome

--- SpatialApplication/src/vendor/fonts/FontIcons.h
+++ SpatialApplication/src/vendor/fonts/FontIcons.h
@@ -5,14 +5,23 @@
 #pragma once
 
 #define FONT_ICON_BUFFER_NAME_jplsa jplsa_compressed_data_base85
-#define FONT_ICON_BUFFER_SIZE_jplsa 0x5d7
+#define FONT_ICON_BUFFER_SIZE_jplsa 0xb68
 
 #define ICON_MIN_jplsa 0x23f8
-#define ICON_MAX_jplsa 0xf07c
+#define ICON_MAX_jplsa 0xf58e
 
 #define ICON_jplsa_ARROWS_ROTATE u8"\uf021"
+#define ICON_jplsa_ELLIPSIS u8"\uf141"
+#define ICON_jplsa_ELLIPSIS_VERTICAL u8"\uf142"
 #define ICON_jplsa_FOLDER u8"\uf07b"
 #define ICON_jplsa_FOLDER_OPEN u8"\uf07c"
+#define ICON_jplsa_GEAR u8"\uf013"
+#define ICON_jplsa_GRIP u8"\uf58d"
+#define ICON_jplsa_GRIP_VERTICAL u8"\uf58e"
 #define ICON_jplsa_PAUSE u8"\u23f8"
 #define ICON_jplsa_PLAY u8"\u25b6"
+#define ICON_jplsa_SLIDERS u8"\uf1de"
 #define ICON_jplsa_STOP u8"\u23f9"
+#define ICON_jplsa_TOGGLE_OFF u8"\uf204"
+#define ICON_jplsa_TOGGLE_ON u8"\uf205"
+
