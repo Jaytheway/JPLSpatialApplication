a0ded4453c26f9ff3189810c5a146638b802b5e2
Jaroslav <music.jp.production@gmail.com>
2026-06-15 14:38:18 +0300

    A few more icons to FontIcons

--- SpatialApplication/src/vendor/fonts/FontIcons.h
+++ SpatialApplication/src/vendor/fonts/FontIcons.h
@@ -5,7 +5,7 @@
 #pragma once
 
 #define FONT_ICON_BUFFER_NAME_jplsa jplsa_compressed_data_base85
-#define FONT_ICON_BUFFER_SIZE_jplsa 0xb68
+#define FONT_ICON_BUFFER_SIZE_jplsa 0xc08
 
 #define ICON_MIN_jplsa 0x23f8
 #define ICON_MAX_jplsa 0xf58e
@@ -18,10 +18,10 @@
 #define ICON_jplsa_GEAR u8"\uf013"
 #define ICON_jplsa_GRIP u8"\uf58d"
 #define ICON_jplsa_GRIP_VERTICAL u8"\uf58e"
+#define ICON_jplsa_MAGNIFYING_GLASS u8"\uf002"
 #define ICON_jplsa_PAUSE u8"\u23f8"
 #define ICON_jplsa_PLAY u8"\u25b6"
 #define ICON_jplsa_SLIDERS u8"\uf1de"
 #define ICON_jplsa_STOP u8"\u23f9"
 #define ICON_jplsa_TOGGLE_OFF u8"\uf204"
 #define ICON_jplsa_TOGGLE_ON u8"\uf205"
-
