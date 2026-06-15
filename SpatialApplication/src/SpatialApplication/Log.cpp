12aa1e6d077184530092650d2b61a38161902b27
Jaroslav <music.jp.production@gmail.com>
2026-06-15 14:55:15 +0300

    Simple `Log` interface

---
+++ SpatialApplication/src/SpatialApplication/Log.cpp
@@ -0,0 +1,132 @@
+﻿//
+//      ██╗██████╗     ██╗     ██╗██████╗ ███████╗
+//      ██║██╔══██╗    ██║     ██║██╔══██╗██╔════╝		** JPL Spatial Application **
+//      ██║██████╔╝    ██║     ██║██████╔╝███████╗
+// ██   ██║██╔═══╝     ██║     ██║██╔══██╗╚════██║		https://github.com/Jaytheway/JPLSpatialApplication
+// ╚█████╔╝██║         ███████╗██║██████╔╝███████║
+//  ╚════╝ ╚═╝         ╚══════╝╚═╝╚═════╝ ╚══════╝
+//
+//   Copyright Jaroslav Pevno 2026, JPL Spatial Application is offered under the terms of the ISC license:
+//
+//   Permission to use, copy, modify, and/or distribute this software for any purpose with or
+//   without fee is hereby granted, provided that the above copyright notice and this permission
+//   notice appear in all copies. THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL
+//   WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY
+//   AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR
+//   CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS,
+//   WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN
+//   CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
+#include "Log.h"
+
+#include <spdlog/sinks/callback_sink.h>
+
+#include <iterator>
+
+namespace JPL
+{
+    ELogLevel Log::TranslateLogLevel(spdlog::level::level_enum level)
+    {
+        switch (level)
+        {
+        case spdlog::level::trace:      return ELogLevel::Trace;
+        case spdlog::level::debug:      return ELogLevel::Debug;
+        case spdlog::level::info:       return ELogLevel::Info;
+        case spdlog::level::warn:       return ELogLevel::Warn;
+        case spdlog::level::err:        return ELogLevel::Error;
+        case spdlog::level::critical:   return ELogLevel::Critical;
+       // case spdlog::level::off:        return ELogLevel::Off;
+        default:
+            return ELogLevel::Trace;
+        }
+    }
+
+    spdlog::level::level_enum Log::TranslateLogLevel(ELogLevel level)
+    {
+        switch (level)
+        {
+        case ELogLevel::Trace:      return spdlog::level::trace;
+        case ELogLevel::Debug:      return spdlog::level::debug;
+        case ELogLevel::Info:       return spdlog::level::info;
+        case ELogLevel::Warn:       return spdlog::level::warn;
+        case ELogLevel::Error:      return spdlog::level::err;
+        case ELogLevel::Critical:   return spdlog::level::critical;
+       // case ELogLevel::Off:        return spdlog::level::off;
+        default:
+            return spdlog::level::trace;
+        }
+    }
+
+    moodycamel::ConsumerToken* gLogConsumerToken = nullptr;
+
+	void Log::Init()
+	{
+        if (JPL_ENSURE(gLogConsumerToken == nullptr))
+            gLogConsumerToken = new moodycamel::ConsumerToken(sQueue);
+
+        // Using single-theraded sink, since we use our own buffer and mutex
+        auto sink = std::make_shared<spdlog::sinks::callback_sink_st>(&LogCallback);
+
+        sLogger = std::make_shared<spdlog::logger>("JPLSpatialApplication", sink);
+        sLogger->set_level(spdlog::level::trace); // receive all messages
+    }
+
+    void Log::Uninit()
+    {
+        sLogger.reset();
+        // We don't need to call spdlog::drop_all(), Walnut does it
+
+        delete gLogConsumerToken;
+        gLogConsumerToken = nullptr;
+    }
+
+    void Log::Trace(std::string_view text)
+    {
+        sLogger->trace(text);
+    }
+
+    void Log::Print(ELogLevel level, std::string_view text)
+    {
+        sLogger->log(TranslateLogLevel(level), text);
+    }
+
+    void Log::Error(std::string_view text)
+    {
+        sLogger->error(text);
+    }
+
+    void Log::Warn(std::string_view text)
+    {
+        sLogger->warn(text);
+    }
+
+    void Log::Info(std::string_view text)
+    {
+        sLogger->info(text);
+    }
+
+    void Log::ExtractLastMessages(std::vector<LogMessage>& outMessages)
+	{
+        outMessages.reserve(outMessages.size() + sQueue.size_approx());
+
+        size_t dequeued = 0;
+        do
+        {
+            static constexpr size_t numToDequeue = 64;
+
+            dequeued = sQueue.try_dequeue_bulk(*gLogConsumerToken,
+                                               std::back_inserter(outMessages),
+                                               numToDequeue);
+        } while (dequeued > 0);
+        
+	}
+
+    void Log::LogCallback(const spdlog::details::log_msg& message)
+    {
+        //! Note: if we want patterns/formatting, we need to do it manually
+        //! since we're using simple callback sink
+        sQueue.enqueue(LogMessage(TranslateLogLevel(message.level),
+                                  std::string(message.payload.begin(), message.payload.end())));
+    }
+
+} // namespace JPL
---
+++ SpatialApplication/src/SpatialApplication/Log.h
@@ -0,0 +1,173 @@
+﻿//
+//      ██╗██████╗     ██╗     ██╗██████╗ ███████╗
+//      ██║██╔══██╗    ██║     ██║██╔══██╗██╔════╝		** JPL Spatial Application **
+//      ██║██████╔╝    ██║     ██║██████╔╝███████╗
+// ██   ██║██╔═══╝     ██║     ██║██╔══██╗╚════██║		https://github.com/Jaytheway/JPLSpatialApplication
+// ╚█████╔╝██║         ███████╗██║██████╔╝███████║
+//  ╚════╝ ╚═╝         ╚══════╝╚═╝╚═════╝ ╚══════╝
+//
+//   Copyright Jaroslav Pevno 2026, JPL Spatial Application is offered under the terms of the ISC license:
+//
+//   Permission to use, copy, modify, and/or distribute this software for any purpose with or
+//   without fee is hereby granted, provided that the above copyright notice and this permission
+//   notice appear in all copies. THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL
+//   WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY
+//   AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR
+//   CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS,
+//   WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN
+//   CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
+#pragma once
+
+#include "LogFormatters.h"
+
+#include "spdlog/spdlog.h"
+
+#include <concurrentqueue.h>
+#include <magic_enum/magic_enum.hpp>
+
+#include <format>
+#include <memory>
+#include <format>
+#include <string>
+#include <string_view>
+#include <vector>
+
+namespace JPL
+{
+	class RingBufferSink;
+
+	enum class ELogLevel : uint8
+	{
+		Trace = 1 << 0,
+		Debug = 1 << 1,
+		Info = 1 << 2,
+		Warn = 1 << 3,
+		Error = 1 << 4,
+		Critical = 1 << 5
+		
+		//Off, // 'Off' value is used to disable spdlog sinks, we don't use it yet
+	};
+
+	// Imporing operators into JPL namespace for convenience
+	namespace EnumFlags
+	{
+		using magic_enum::bitwise_operators::operator~;
+		using magic_enum::bitwise_operators::operator|;
+		using magic_enum::bitwise_operators::operator&;
+		using magic_enum::bitwise_operators::operator^;
+		using magic_enum::bitwise_operators::operator|=;
+		using magic_enum::bitwise_operators::operator&=;
+		using magic_enum::bitwise_operators::operator^=;
+	}
+}
+
+// Lets magic_enum treat ELogLevel as flags
+template <>
+struct magic_enum::customize::enum_range<JPL::ELogLevel>
+{
+	static constexpr bool is_flags = true;
+};
+
+namespace JPL
+{
+	//==========================================================================
+	/// Data we store about messages comming into log.
+	/// (may be extended later with timestamp and other info)
+	struct LogMessage
+	{
+		ELogLevel Level;
+		std::string Message;
+	};
+
+	//==========================================================================
+	/// Simple logger, using spdlog internally and lock-free concurrent queue.
+	/// Safe to use from multiple threads.
+	class Log
+	{
+	public:
+		static void Init();
+		static void Uninit();
+
+		static void Print(ELogLevel level, std::string_view text);
+		static void Trace(std::string_view text);
+		static void Error(std::string_view text);
+		static void Warn(std::string_view text);
+		static void Info(std::string_view text);
+
+		template<class... Args> requires(sizeof...(Args) > 0)
+		static void Print(ELogLevel level, std::format_string<Args...> format, Args&&... args);
+
+		template<class... Args> requires(sizeof...(Args) > 0)
+		static void Trace(std::format_string<Args...> format, Args&&... args);
+
+		template<class... Args> requires(sizeof...(Args) > 0)
+		static void Error(std::format_string<Args...> format, Args&&... args);
+
+		template<class... Args> requires(sizeof...(Args) > 0)
+		static void Warn(std::format_string<Args...> format, Args&&... args);
+
+		template<class... Args> requires(sizeof...(Args) > 0)
+		static void Info(std::format_string<Args...> format, Args&&... args);
+
+		/// Extract all the messages from the queue
+		static void ExtractLastMessages(std::vector<LogMessage>& outMessages);
+
+	private:
+		static void LogCallback(const spdlog::details::log_msg& message);
+
+		static ELogLevel TranslateLogLevel(spdlog::level::level_enum level);
+		static spdlog::level::level_enum TranslateLogLevel(ELogLevel level);
+
+	private:
+		inline static std::shared_ptr<::spdlog::logger> sLogger;
+
+		static constexpr std::size_t cInitialQueueSize = 1024;
+		inline static moodycamel::ConcurrentQueue<LogMessage> sQueue{ cInitialQueueSize };
+	};
+
+	inline std::string_view LogLevelToString(ELogLevel level)
+	{
+		return magic_enum::enum_name(level);
+	}
+} // namespace JPL
+
+//==============================================================================
+//
+//   Code beyond this point is implementation detail...
+//
+//==============================================================================
+
+namespace JPL
+{
+	template<class ...Args> requires(sizeof...(Args) > 0)
+	inline void Log::Print(ELogLevel level, std::format_string<Args...> format, Args && ...args)
+	{
+		sLogger->log(TranslateLogLevel(level), std::format(format, std::forward<Args>(args)...));
+	}
+
+	template<class ...Args> requires(sizeof...(Args) > 0)
+	inline void Log::Trace(std::format_string<Args...> format, Args&& ...args)
+	{
+		sLogger->trace(std::format(format, std::forward<Args>(args)...));
+	}
+
+	template<class ...Args> requires(sizeof...(Args) > 0)
+	inline void Log::Error(std::format_string<Args...> format, Args && ...args)
+	{
+		sLogger->error(std::format(format, std::forward<Args>(args)...));
+	}
+
+	template<class ...Args> requires(sizeof...(Args) > 0)
+	inline void Log::Warn(std::format_string<Args...> format, Args && ...args)
+	{
+		sLogger->warn(std::format(format, std::forward<Args>(args)...));
+	}
+
+	template<class ...Args> requires(sizeof...(Args) > 0)
+	inline void Log::Info(std::format_string<Args...> format, Args && ...args)
+	{
+		sLogger->info(std::format(format, std::forward<Args>(args)...));
+	}
+
+} // namespace JPL
