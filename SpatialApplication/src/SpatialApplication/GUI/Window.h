b5420c4c7580301f613ea82e1581d4d989a2d58f
Jaroslav <music.jp.production@gmail.com>
2026-07-10 17:40:51 +0300

    `GUI::Window` that can be open/closed from menu
    - At the moment it simply uses windwow state from `JPLSpatialApplication` for open/close state global access and serialization.

---
+++ SpatialApplication/src/SpatialApplication/GUI/Window.h
@@ -0,0 +1,42 @@
+﻿//
+//      ██╗██████╗     ██╗     ██╗██████╗ ███████╗
+//      ██║██╔══██╗    ██║     ██║██╔══██╗██╔════╝		** JPL Spatial Application **
+//      ██║██████╔╝    ██║     ██║██████╔╝███████╗
+// ██   ██║██╔═══╝     ██║     ██║██╔══██╗╚════██║		https://github.com/Jaytheway/JPLSpatialApplication
+// ╚█████╔╝██║         ███████╗██║██████╔╝███████║
+//  ╚════╝ ╚═╝         ╚══════╝╚═╝╚═════╝ ╚══════╝
+//
+//   Copyright 2026 Jaroslav Pevno, JPL Spatial Application is offered under the terms of the ISC license:
+//
+//   Permission to use, copy, modify, and/or distribute this software for any purpose with or
+//   without fee is hereby granted, provided that the above copyright notice and this permission
+//   notice appear in all copies. THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL
+//   WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY
+//   AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR
+//   CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS,
+//   WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN
+//   CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
+#pragma once
+
+#include "Application.h"
+#include "ImGui/ImGui.h"
+
+namespace JPL::GUI
+{
+
+	template<class DrawFunction>
+	void Window(const char* name, const DrawFunction& draw, const ImGuiEx::WindowConfig& config = {})
+	{
+		auto& windowState = JPLSpatialApplication::GetWindowState(name);
+		
+		ImGuiEx::Window(name, draw, &windowState.bOpen, config);
+	}
+
+	template<class DrawFunction>
+	void Window(const char* name, const ImGuiEx::WindowConfig& config, const DrawFunction& draw)
+	{
+		Window(name, draw, config);
+	}
+
+} // namespace JPL::GUI
